ecf78ce8fee6350627b9f83cc19c546c54b5f6ec
Exz-Laos <xbecomputer12345@gmail.com>
2024-08-20 10:25:10 +0900

    first commit with Header and Login

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import loginIcons from '../assest/signin.gif'
+import { FaEye } from "react-icons/fa";
+const Login = () => {
+  return (
+    <section id='login'>
+        <div className='mx-auto container p-4'>
+            <div className='bg-white p-2 py-5 w-full max-w-md mx-auto' >
+                 <div className='w-20 h-20 mx-auto'>
+                    <img src={loginIcons} alt='login icons'/>
+                 </div>
+
+                 <form>
+                    <div className='grid'>
+                        <label>Email: </label>
+                        <div className='bg-slate-100  '>
+                            <input type='email' placeholder='enter email' 
+                            className='w-full h-full outline-none bg-transparent'/>
+                        </div>
+                    </div>
+                    <div>
+                        <label>Password:</label>
+                        <div className='bg-slate-100  ' >
+                            <input type='password' placeholder='enter password' 
+                            className='w-full h-full outline-none bg-transparent'/>
+                        </div>
+                    </div>
+
+                    <button>Login</button>
+                 </form>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Login
